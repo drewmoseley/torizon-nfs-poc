f3cc2884c044ed5d028985c5fe74b94082893859
Drew Moseley <drew@moseleynet.net>
2022-10-25 15:16:10 -0400

    Refactor into bash functions
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- doit.sh
+++ doit.sh
@@ -1,203 +1,28 @@
 #!/bin/bash
 
-for app in wget curl jq; do
-    if [ -z "$(type -path $app)" ]; then
-        echo Please install $app
-        set +x
-        exit 1
-    fi
-done
+source $(dirname $0)/utils.sh
+                              
+verify_installed_apps
+create_or_verify_config
+setup_torizoncore_builder
+setup_server_config_files
 
-if [ ! -e config.sh ]; then
-    cat > config.sh <<EOF
-server_ip=
-server_config_dir_prefix=
-server_machine=
-client_ip=
-client_config_dir_prefix=
-client_machine=
-usb_key=
-api_client_id=
-api_client_secret=
-EOF
-    echo "Please edit config.sh and set parameter values"
-    exit 1
-fi
-
-source config.sh
-if [ -z "${client_ip}" ]; then
-    echo Please add client_ip to config.sh with the IP address or FQDN of the secondary/client board
-    exit 1
-fi
-
-if [ -z "${server_ip}" ]; then
-    echo Please add server_ip to config.sh with the IP address or FQDN of the primary/NFS server board
-    exit 1
-fi
-
-if [ -z "${usb_key}" ]; then
-    echo Please add usb_key to config.sh with the name of the USB key
-    exit 1
-fi
-
-if [ -z "${api_client_id}" ] || [ -z "${api_client_secret}" ]; then
-    echo Please add API credentials from  https://app.torizon.io/#/account to config.sh
-    exit 1
-fi
-
-if [ -z "${server_config_dir_prefix}" ]; then
-    echo Please set server_config_dir_prefix in config.sh
-    exit 1
-fi
-
-if [ -z "${client_config_dir_prefix}" ]; then
-    echo Please set client_config_dir_prefix in config.sh
-    exit 1
-fi
-
-if [ -z "${server_machine}" ]; then
-    echo Please set server_machine in config.sh
-    exit 1
-fi
-
-if [ -z "${client_machine}" ]; then
-    echo Please set client_machine in config.sh
-    exit 1
-fi
-
-if [ ! -e credentials.zip ]; then
-    echo Please download account credentials from \"https://app.torizon.io/#/account\" to \"$(pwd)/credentials.zip\"
-    exit 1
-fi
-
-if [ ! -e tcb-env-setup.sh ]; then
-    wget https://raw.githubusercontent.com/toradex/tcb-env-setup/master/tcb-env-setup.sh
-fi
-source ./tcb-env-setup.sh -a remote
-shopt -s expand_aliases
-
-if [ ! -e shared-data.tar.gz ]; then
-    torizoncore-builder platform provisioning-data \
-                        --credentials credentials.zip \
-                        --shared-data shared-data.tar.gz
-fi
-
-cp -f shared-data.tar.gz credentials.zip ${server_config_dir_prefix}_v1/
-cp -f shared-data.tar.gz credentials.zip ${server_config_dir_prefix}_update/
-cp -f shared-data.tar.gz credentials.zip ${client_config_dir_prefix}_v1/
-cp -f shared-data.tar.gz credentials.zip ${client_config_dir_prefix}_update/
-
-sed -s "s~@server-ip@~${server_ip}~" nfs.mount.in > ${client_config_dir_prefix}_v1/changes/usr/etc/systemd/system/nfs.mount
-sed -s "s~@server-ip@~${server_ip}~" nfs.mount.in > ${client_config_dir_prefix}_update/changes/usr/etc/systemd/system/nfs.mount
-mkdir -p ${client_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/
-mkdir -p ${client_config_dir_prefix}_update/changes/usr/etc/sota/conf.d/
-sed -e "s~@offline-update-path@~/nfs/update~" \
-    100-offline-updates.toml.in \
-    > ${client_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/100-offline-updates.toml
-sed -e "s~@offline-update-path@~/nfs/update~" \
-    100-offline-updates.toml.in \
-    > ${client_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/100-offline-updates.toml
-
-sed -e "s~@client-ip@~${client_ip}~" exports.in > ${server_config_dir_prefix}_v1/changes/usr/etc/exports
-sed -e "s~@client-ip@~${client_ip}~" exports.in > ${server_config_dir_prefix}_update/changes/usr/etc/exports
-mkdir -p ${server_config_dir_prefix}_v1/changes/nfs
-mkdir -p ${server_config_dir_prefix}_update/changes/nfs
-mkdir -p ${server_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/
-mkdir -p ${server_config_dir_prefix}_update/changes/usr/etc/sota/conf.d/
-sed -e "s~@offline-update-path@~/var/rootdirs/media/${usb_key}/update~" \
-    100-offline-updates.toml.in \
-    > ${server_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/100-offline-updates.toml
-sed -e "s~@offline-update-path@~/var/rootdirs/media/${usb_key}/update~" \
-    100-offline-updates.toml.in \
-    > ${server_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/100-offline-updates.toml
-
-mkdir -p ${client_config_dir_prefix}_v1/changes/usr/etc/systemd/system/multi-user.target.wants
-mkdir -p ${client_config_dir_prefix}_update/changes/usr/etc/systemd/system/multi-user.target.wants
-ln -s /etc/systemd/system/multi-user.target.wants/nfs.mount \
-   ${client_config_dir_prefix}_v1/changes/usr/etc/systemd/system/multi-user.target.wants/nfs.mount
-ln -s /etc/systemd/system/multi-user.target.wants/nfs.mount \
-   ${client_config_dir_prefix}_update/changes/usr/etc/systemd/system/multi-user.target.wants/nfs.mount
-
-for MACHINE_CONFIG in ${server_config_dir_prefix}_v1 ${client_config_dir_prefix}_v1; do
-    (
-        cd $MACHINE_CONFIG
-        rm -rf tezi
-        torizoncore-builder build
-    )
-done
+run_torizoncore_builder -h
+exit 1
 
 TDX_TOKEN=$(curl -s https://kc.torizon.io/auth/realms/ota-users/protocol/openid-connect/token \
 				 -d client_id=${api_client_id} -d client_secret=${api_client_secret} \
 				 -d grant_type=client_credentials | jq -r .access_token)
-
-# Make sure ${client_config_dir_prefix}_update is first in the list so that it
-# can be built and included in the build of ${server_config_dir_prefix}_update
 PACKAGE_VERSION=1
 EXPIRATION_DATE=$(date -d "+7 days" -u +%Y-%m-%dT%H:%M:%SZ)
 
-for FULL_MACHINE_CONFIG in ${client_machine}:${client_config_dir_prefix}_update ${server_machine}:${server_config_dir_prefix}_update; do
-    (
-        TORIZON_MACHINE=$(echo ${FULL_MACHINE_CONFIG} | cut -d: -f1)
-        CONFIG_DIR=$(echo ${FULL_MACHINE_CONFIG} | cut -d: -f2)
-        MACHINE_CONFIG=${CONFIG_DIR}
-        cd ${CONFIG_DIR}
-
-        rm -rf tezi
-
-        if [ -e "build.hash" ]; then
-            echo Deleting image ${MACHINE_CONFIG}-$(cat build.hash)
-            curl -s --header "Authorization: Bearer ${TDX_TOKEN}" \
-			     --header "Content-Type: application/json" \
-			     --location \
-			     --request DELETE https://app.torizon.io/api/v1/user_repo/targets/${MACHINE_CONFIG}-$(cat build.hash) || true
-        fi
-
-        torizoncore-builder build 2>&1 | tee build.out
-        grep 'Deploying OSTree with checksum' build.out  | awk '{print $NF}' | tr -d '[:space:]' > build.hash
-        rm -f build.out
-
-	    torizoncore-builder platform push \
-			--credentials credentials.zip \
-			--package-name "${MACHINE_CONFIG}" \
-			--package-version "${PACKAGE_VERSION}" \
-			"${MACHINE_CONFIG}"
-
-        BUILD_HASH=$(cat build.hash)
-        PACKAGE_LENGTH=$(curl -s --header "Authorization: Bearer ${TDX_TOKEN}" --location \
-					          --request GET https://app.torizon.io/api/v1/user_repo/targets.json | \
-					         jq ".signed.targets[\"${MACHINE_CONFIG}-${BUILD_HASH}\"].length")
-
-        echo Creating Lockbox
-        read -d '' LOCKBOX_BODY << EOF
-{
-  "expiresAt": "${EXPIRATION_DATE}",
-  "values": {
-    "${MACHINE_CONFIG}-${BUILD_HASH}": {
-      "hashes": {
-        "sha256": "${BUILD_HASH}"
-      },
-      "length": ${PACKAGE_LENGTH},
-      "custom": {
-        "hardwareIds": [
-          "${TORIZON_MACHINE}"
-        ]
-      }
-    }
-  }
-}
-EOF
-        
-	    curl -s --header "Authorization: Bearer ${TDX_TOKEN}" \
-			--header "Content-Type: application/json" \
-			--location \
-			--request POST https://app.torizon.io/api/v1/admin/repo/offline-updates/${MACHINE_CONFIG}_${PACKAGE_VERSION} \
-			--data "${LOCKBOX_BODY}"
-
-        echo Creating Update media
-	    torizoncore-builder platform lockbox \
-			   "${MACHINE_CONFIG}_${PACKAGE_VERSION}" \
-			   --credentials credentials.zip \
-			   --output-directory update \
-			   --force
-    )
-done
+torizoncore_builder_build ${server_config_dir_prefix}_v1 ${TDX_TOKEN}
+torizoncore_builder_build ${client_config_dir_prefix}_v1 ${TDX_TOKEN}
+torizoncore_builder_build ${server_config_dir_prefix}_update ${TDX_TOKEN}
+torizoncore_builder_build ${client_config_dir_prefix}_update ${TDX_TOKEN}
+torizoncore_builder_push  ${server_config_dir_prefix}_update ${PACKAGE_VERSION}
+torizoncore_builder_push  ${client_config_dir_prefix}_update ${PACKAGE_VERSION}
+torizoncore_builder_define_lockbox ${client_config_dir_prefix}_update ${TDX_TOKEN} ${PACKAGE_VERSION} ${EXPIRATION_DATE} ${client_machine}
+torizoncore_builder_define_lockbox ${server_config_dir_prefix}_update ${TDX_TOKEN} ${PACKAGE_VERSION} ${EXPIRATION_DATE} ${server_machine}
+torizoncore_builder_build_lockbox ${client_config_dir_prefix}_update ${PACKAGE_VERSION}
+torizoncore_builder_build_lockbox ${server_config_dir_prefix}_update ${PACKAGE_VERSION}
---
+++ utils.sh
@@ -0,0 +1,251 @@
+SOURCED=0
+if [ -n "$BASH_VERSION" ]; then
+    (return 0 2>/dev/null) && SOURCED=1
+fi
+if [ "$SOURCED" = "0" ]; then
+    echo "Error: don't run $0, source it."
+    exit 1
+fi
+
+verify_installed_apps() {
+    for app in wget curl jq; do
+        if [ -z "$(type -path $app)" ]; then
+            echo Please install $app
+            set +x
+            exit 1
+        fi
+    done
+}
+
+create_or_verify_config() {
+    if [ ! -e config.sh ]; then
+        cat > config.sh <<EOF
+server_ip=
+server_config_dir_prefix=
+server_machine=
+client_ip=
+client_config_dir_prefix=
+client_machine=
+usb_key=
+api_client_id=
+api_client_secret=
+EOF
+        echo "Please edit config.sh and set parameter values"
+        exit 1
+    fi
+
+    source config.sh
+    if [ -z "${client_ip}" ]; then
+        echo Please add client_ip to config.sh with the IP address or FQDN of the secondary/client board
+        exit 1
+    fi
+
+    if [ -z "${server_ip}" ]; then
+        echo Please add server_ip to config.sh with the IP address or FQDN of the primary/NFS server board
+        exit 1
+    fi
+
+    if [ -z "${usb_key}" ]; then
+        echo Please add usb_key to config.sh with the name of the USB key
+        exit 1
+    fi
+
+    if [ -z "${api_client_id}" ] || [ -z "${api_client_secret}" ]; then
+        echo Please add API credentials from  https://app.torizon.io/#/account to config.sh
+        exit 1
+    fi
+
+    if [ -z "${server_config_dir_prefix}" ]; then
+        echo Please set server_config_dir_prefix in config.sh
+        exit 1
+    fi
+
+    if [ -z "${client_config_dir_prefix}" ]; then
+        echo Please set client_config_dir_prefix in config.sh
+        exit 1
+    fi
+
+    if [ -z "${server_machine}" ]; then
+        echo Please set server_machine in config.sh
+        exit 1
+    fi
+
+    if [ -z "${client_machine}" ]; then
+        echo Please set client_machine in config.sh
+        exit 1
+    fi
+}
+
+setup_torizoncore_builder() {
+    if [ ! -e credentials.zip ]; then
+        echo Please download account credentials from \"https://app.torizon.io/#/account\" to \"$(pwd)/credentials.zip\"
+        exit 1
+    fi
+
+    if [ ! -e tcb-env-setup.sh ]; then
+        wget https://raw.githubusercontent.com/toradex/tcb-env-setup/master/tcb-env-setup.sh
+    fi
+
+    source ./tcb-env-setup.sh -a remote
+    run_torizoncore_builder -h
+    # get_torizon_shared_data
+}
+
+# Explicitly run torizoncore-builder. We do it inside this function
+# because it does not export out to the parent shell environment
+run_torizoncore_builder() {
+    source ./tcb-env-setup.sh -a local
+    shopt -s expand_aliases
+    torizoncore-builder $*
+}
+
+get_torizon_shared_data() {
+    if [ ! -e shared-data.tar.gz ]; then
+        run_torizoncore_builder platform provisioning-data \
+                                --credentials credentials.zip \
+                                --shared-data shared-data.tar.gz
+    fi
+
+    cp -f shared-data.tar.gz credentials.zip ${server_config_dir_prefix}_v1/
+    cp -f shared-data.tar.gz credentials.zip ${server_config_dir_prefix}_update/
+    cp -f shared-data.tar.gz credentials.zip ${client_config_dir_prefix}_v1/
+    cp -f shared-data.tar.gz credentials.zip ${client_config_dir_prefix}_update/
+}
+
+setup_server_config_files() {
+    sed -s "s~@server-ip@~${server_ip}~" nfs.mount.in > ${client_config_dir_prefix}_v1/changes/usr/etc/systemd/system/nfs.mount
+    sed -s "s~@server-ip@~${server_ip}~" nfs.mount.in > ${client_config_dir_prefix}_update/changes/usr/etc/systemd/system/nfs.mount
+    mkdir -p ${client_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/
+    mkdir -p ${client_config_dir_prefix}_update/changes/usr/etc/sota/conf.d/
+    sed -e "s~@offline-update-path@~/nfs/update~" \
+        100-offline-updates.toml.in \
+        > ${client_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/100-offline-updates.toml
+    sed -e "s~@offline-update-path@~/nfs/update~" \
+        100-offline-updates.toml.in \
+        > ${client_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/100-offline-updates.toml
+}
+
+setup_client_config_files() {
+    sed -e "s~@client-ip@~${client_ip}~" exports.in > ${server_config_dir_prefix}_v1/changes/usr/etc/exports
+    sed -e "s~@client-ip@~${client_ip}~" exports.in > ${server_config_dir_prefix}_update/changes/usr/etc/exports
+    mkdir -p ${server_config_dir_prefix}_v1/changes/nfs
+    mkdir -p ${server_config_dir_prefix}_update/changes/nfs
+    mkdir -p ${server_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/
+    mkdir -p ${server_config_dir_prefix}_update/changes/usr/etc/sota/conf.d/
+    sed -e "s~@offline-update-path@~/var/rootdirs/media/${usb_key}/update~" \
+        100-offline-updates.toml.in \
+        > ${server_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/100-offline-updates.toml
+    sed -e "s~@offline-update-path@~/var/rootdirs/media/${usb_key}/update~" \
+        100-offline-updates.toml.in \
+        > ${server_config_dir_prefix}_v1/changes/usr/etc/sota/conf.d/100-offline-updates.toml
+
+    mkdir -p ${client_config_dir_prefix}_v1/changes/usr/etc/systemd/system/multi-user.target.wants
+    mkdir -p ${client_config_dir_prefix}_update/changes/usr/etc/systemd/system/multi-user.target.wants
+    ln -s /etc/systemd/system/multi-user.target.wants/nfs.mount \
+       ${client_config_dir_prefix}_v1/changes/usr/etc/systemd/system/multi-user.target.wants/nfs.mount
+    ln -s /etc/systemd/system/multi-user.target.wants/nfs.mount \
+       ${client_config_dir_prefix}_update/changes/usr/etc/systemd/system/multi-user.target.wants/nfs.mount
+}
+
+torizoncore_builder_build() {
+    if [ $# != 2 ]; then
+        echo "Usage: ${FUNCNAME[0]} MACHINE_CONFIG_DIR TDX_TOKEN"
+        exit 1
+    fi
+    local machine_config="${1}"
+    local tdx_token="${2}"
+    
+    cd ${machine_config}
+    rm -rf tezi
+    if [ -e "build.hash" ]; then
+        local build_hash=$(cat build.hash)
+        echo Deleting image ${machine_config}-${build_hash}
+        curl -s --header "Authorization: Bearer ${TDX_TOKEN}" \
+			 --header "Content-Type: application/json" \
+			 --location \
+			 --request DELETE https://app.torizon.io/api/v1/user_repo/targets/${machine_config}-${build_hash} || true
+    fi
+    torizoncore-builder build 2>&1 | tee build.out
+    grep 'Deploying OSTree with checksum' build.out  | awk '{print $NF}' | tr -d '[:space:]' > build.hash
+    rm -f build.out
+    cd -
+}
+
+torizoncore_builder_push() {
+    if [ $# != 3 ]; then
+        echo "Usage: ${FUNCNAME[0]} MACHINE_CONFIG_DIR PACKAGE_VERSION"
+        exit 1
+    fi
+    local machine_config="${1}"
+    local package_version="${2}"
+    
+    cd ${machine_config}
+	torizoncore-builder platform push \
+			            --credentials credentials.zip \
+			            --package-name "${machine_config}" \
+			            --package-version "${package_version}" \
+			            "${machine_config}"
+    cd -
+}
+
+# Use the server API to _define_ the lockbox.
+# currently tcb does not have this functionality
+torizoncore_builder_define_lockbox() {
+    if [ $# != 3 ]; then
+        echo "Usage: ${FUNCNAME[0]} MACHINE_CONFIG_DIR TDX_TOKEN PACKAGE_VERSION TORIZON_MACHINE"
+        exit 1
+    fi
+    local machine_config="${1}"
+    local tdx_token="${2}"
+    local package_version="${3}"
+    local torizon_machine="${4}"
+
+    cd ${machine_config}
+    local build_hash=$(cat build.hash)
+    local package_length=$(curl -s --header "Authorization: Bearer ${TDX_TOKEN}" --location \
+					            --request GET https://app.torizon.io/api/v1/user_repo/targets.json | \
+					           jq ".signed.targets[\"${machine_config}-${build_hash}\"].length")
+
+    echo Creating Lockbox
+    read -d '' lockbox_body << EOF
+{
+  "expiresAt": "${expiration_date}",
+  "values": {
+    "${machine_config}-${build_hash}": {
+      "hashes": {
+        "sha256": "${build_hash}"
+      },
+      "length": ${package_length},
+      "custom": {
+        "hardwareIds": [
+          "${torizon_machine}"
+        ]
+      }
+    }
+  }
+}
+EOF
+    
+	curl -s --header "Authorization: Bearer ${TDX_TOKEN}" \
+		 --header "Content-Type: application/json" \
+		 --location \
+		 --request POST https://app.torizon.io/api/v1/admin/repo/offline-updates/${machine_config}_${package_version} \
+		 --data "${lockbox_body}"
+    cd -
+}
+
+torizoncore_builder_build_lockbox() {
+    if [ $# != 2 ]; then
+        echo "Usage: ${FUNCNAME[0]} MACHINE_CONFIG_DIR PACKAGE_VERSION"
+        exit 1
+    fi
+    local machine_config="${1}"
+    local package_version="${2}"
+    cd ${machine_config}
+	torizoncore-builder platform lockbox \
+			            "${machine_config}_${package_version}" \
+			            --credentials credentials.zip \
+			            --output-directory update \
+			            --force
+    cd -
+}
