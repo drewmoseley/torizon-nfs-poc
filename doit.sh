1734675db5f0c2f12f572f8f2b449d2b1cb6da71
Drew Moseley <drew@moseleynet.net>
2022-11-18 08:40:03 -0500

    Build the client lockbox first and include it in the server update image
    Signed-off-by: Drew Moseley <drew@moseleynet.net>

--- doit.sh
+++ doit.sh
@@ -9,13 +9,22 @@ setup_torizoncore_builder
 setup_server_config_files
 setup_client_config_files
 
+# Build both V1 images
 torizoncore_builder_build ${server_config_dir_prefix}_v1 ${TDX_TOKEN}
 torizoncore_builder_build ${client_config_dir_prefix}_v1 ${TDX_TOKEN}
-torizoncore_builder_build ${server_config_dir_prefix}_update ${TDX_TOKEN}
+
+# Build the client update image and lockbox
 torizoncore_builder_build ${client_config_dir_prefix}_update ${TDX_TOKEN}
-torizoncore_builder_push  ${server_config_dir_prefix}_update ${package_version}
 torizoncore_builder_push  ${client_config_dir_prefix}_update ${package_version}
 torizoncore_builder_define_lockbox ${client_config_dir_prefix}_update ${TDX_TOKEN} ${EXPIRATION_DATE} ${client_machine}
-torizoncore_builder_define_lockbox ${server_config_dir_prefix}_update ${TDX_TOKEN} ${EXPIRATION_DATE} ${server_machine}
 torizoncore_builder_build_lockbox ${client_config_dir_prefix}_update
+
+# Copy the lockbox into the server changes directory for inclusion in its image
+mkdir -p ${server_config_dir_prefix}_update/changes/nfs
+cp -R ${client_config_dir_prefix}_update/update ${server_config_dir_prefix}_update/changes/nfs/update_secondary
+
+# Build the server update image and lockbox containing the client update lockbox
+torizoncore_builder_build ${server_config_dir_prefix}_update ${TDX_TOKEN}
+torizoncore_builder_push  ${server_config_dir_prefix}_update ${package_version}
+torizoncore_builder_define_lockbox ${server_config_dir_prefix}_update ${TDX_TOKEN} ${EXPIRATION_DATE} ${server_machine}
 torizoncore_builder_build_lockbox ${server_config_dir_prefix}_update
